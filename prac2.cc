2f6030db50a31ef8d80c365eb74efbed293a2281
ycc13-ua <98881719+ycc13-ua@users.noreply.github.com>
2022-03-14 10:23:23 +0100

    Add files via upload

---
+++ prac2.cc
@@ -0,0 +1,157 @@
+// DNI X8866729-E CHEN,YANAN
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+const int KMAXSTRING = 50;
+
+enum Error {
+  ERR_OPTION,
+  ERR_BOOK_TITLE,
+  ERR_BOOK_AUTHORS,
+  ERR_BOOK_DATE,
+  ERR_BOOK_PRICE,
+  ERR_ID,
+  ERR_FILE,
+  ERR_ARGS
+};
+
+struct Book {
+  unsigned int id;
+  string title;
+  string authors;
+  int year;
+  string slug;
+  float price;
+};
+
+struct BinBook {
+  unsigned int id;
+  char title[KMAXSTRING];
+  char authors[KMAXSTRING];
+  int year;
+  char slug[KMAXSTRING];
+  float price;
+};
+
+struct BookStore {
+  string name;
+  vector<Book> books;
+  unsigned int nextId;
+};
+
+struct BinBookStore {
+  char name[KMAXSTRING];
+  unsigned int nextId;
+};
+
+void error(Error e) {
+  switch (e) {
+    case ERR_OPTION:
+      cout << "ERROR: wrong menu option" << endl;
+      break;
+    case ERR_BOOK_TITLE:
+      cout << "ERROR: wrong book title" << endl;
+      break;
+    case ERR_BOOK_AUTHORS:
+      cout << "ERROR: wrong author(s)" << endl;
+      break;
+    case ERR_BOOK_DATE:
+      cout << "ERROR: wrong date" << endl;
+      break;
+    case ERR_BOOK_PRICE:
+      cout << "ERROR: wrong price" << endl;
+      break;
+    case ERR_ID:
+      cout << "ERROR: wrong book id" << endl;
+      break;
+    case ERR_FILE:
+      cout << "ERROR: cannot open file" << endl;
+      break;
+    case ERR_ARGS:
+      cout << "ERROR: wrong arguments" << endl;
+      break;
+  }
+}
+
+void showMainMenu() {
+  cout << "[Options]" << endl
+       << "1- Show catalog" << endl
+       << "2- Show extended catalog" << endl
+       << "3- Add book" << endl
+       << "4- Delete book" << endl
+       << "5- Import/export" << endl
+       << "q- Quit" << endl
+       << "Option: ";
+}
+
+void showCatalog(const BookStore &bookStore) {
+     for (int i=0 ; i<(int) ; i++){
+          books[i];
+
+     }
+}
+
+void showExtendedCatalog(const BookStore &bookStore) {
+}
+
+void addBook(BookStore &bookStore) {
+}
+
+void deleteBook(BookStore &bookStore) {
+}
+
+void importExportMenu(BookStore &bookStore) {
+}
+
+void importFromCsv(BookStore &bookStore){
+}
+
+void exportToCsv(const BookStore &bookStore){
+}
+
+void loadData(BookStore &bookStore){
+}
+
+void saveData(const BookStore &bookStore){
+}
+
+int main(int argc, char *argv[]) {
+  BookStore bookStore;
+  bookStore.name = "My Book Store";
+  bookStore.nextId = 1;
+
+  char option;
+
+  do {
+    showMainMenu();
+    cin >> option;
+    cin.get();
+
+    switch (option) {
+      case '1':
+        showCatalog(bookStore);
+        break;
+      case '2':
+        showExtendedCatalog(bookStore);
+        break;
+      case '3':
+        addBook(bookStore);
+        break;
+      case '4':
+        deleteBook(bookStore);
+        break;
+      case '5':
+        importExportMenu(bookStore);
+        break;
+      case 'q':
+        break;
+      default:
+        error(ERR_OPTION);
+    }
+  } while (option != 'q');
+
+  return 0;
+}
